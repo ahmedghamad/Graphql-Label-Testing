1c6c40b145a8b2d65d9a3071c8196e7508e3be7d
Ahmed Hamad <a.g.hamad@icloud.com>
2025-11-21 10:21:33 +0000

    updated the read label feature

--- src/test/java/Runner/TestRunners.java
+++ src/test/java/Runner/TestRunners.java
@@ -8,9 +8,12 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = "src/test/resources/features/UpdateLabel.feature",
+        features = "src/test/resources/features",
         glue = "steps",
-        plugin = {"pretty"}
+        plugin = {"pretty",
+                "json:target/cucumber.json",
+                "junit:target/cucumber.xml"
+        }
 )
 public class TestRunners {
 }
